95b458b5f43830b2d0e1869cbba0d5bc877252b1
totoro2048 <totoro2048@naver.com>
2019-12-27 18:09:21 +0900

    라우터미드레어 들록

--- app1.js
+++ app1.js
@@ -3,11 +3,17 @@ var app = express()
 var path = require('path')
 require('dotenv').config()
 
+var indexrouter = require('./router/index.js')
+var userrouter = require('./router/user.js')
+
 app.set('views',path.join(__dirname,'views'))
 app.set('view engine','ejs')
 app.set('html',require('ejs').renderFile)
 app.ues(express.json())
 app.use(express.static(path.join(__dirname,'pubilc')))
+
+app.use('/',indexrouter)
+app.use('/people',userrouter)
 
 
 var port = process.env.port || 3000;
